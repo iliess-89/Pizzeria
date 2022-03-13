ab9b6baa1701eae9ed56f4f490b7bbba47d2f2c6
iliess-89 <mazouariiliess7@gmail.com>
2022-03-13 12:57:20 +0100

    site

---
+++ interaction.js
@@ -0,0 +1,13 @@
+// --nav bar--
+let modal = document.querySelectorAll(".nav");
+let span = document.querySelector(".span2");
+
+for(let modals of modal){
+span.addEventListener("click", nav);
+
+function nav(){
+    modals.classList.toggle("brouette");
+   
+}
+}
+// -- fin nav bar--
